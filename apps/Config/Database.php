b4328757088d9156d47e8b634d6cbe2a5f166d01
aldihamidi <aldi1hamidi2lbs3@gmail.com>
2021-03-10 14:16:01 +0700

    start

--- apps/Config/Database.php
+++ apps/Config/Database.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Config;
 
-class Database {
+class Database
+{
     public static string $host = "localhost";
     public static string $username = "root";
     public static string $password = "";
-    public static string $db   = "event";
+    public static string $db   = "db_cms";
 }
