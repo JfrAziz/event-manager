c1ac7a125c2152b8f9fee258b1715d1c762ffadf
aldihamidi <aldi1hamidi2lbs3@gmail.com>
2021-03-12 23:42:06 +0700

    ubah nama database dari db_cms ke event

--- apps/Config/Database.php
+++ apps/Config/Database.php
@@ -7,5 +7,5 @@ class Database
     public static string $host = "localhost";
     public static string $username = "root";
     public static string $password = "";
-    public static string $db   = "db_cms";
+    public static string $db   = "event";
 }
