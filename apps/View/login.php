cf472ec22ec8a1fdaa6ee26d6e3be7c8ba045e15
aldihamidi <aldi1hamidi2lbs3@gmail.com>
2021-03-29 17:01:47 +0700

    add show password

--- apps/View/login.php
+++ apps/View/login.php
@@ -2,7 +2,7 @@
 
 <?php include_once "_partials/head.php" ?>
 
-<body class="">
+<body class="" style="display: flex; align-items: center">
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-9 col-lg-12 col-xl-10">
@@ -21,8 +21,14 @@
                     <div class="form-group">
                       <input class="form-control form-control-user" type="text" placeholder="Username" name="username">
                     </div>
-                    <div class="form-group">
-                      <input class="form-control form-control-user" type="password" placeholder="Password" name="password">
+                    <div style="position: relative;" class="form-group">
+                      <input class="form-control input-sandi form-control-user" type="password" placeholder="Kata Sandi" name="password">
+                      <div class="eye" style="user-select: none;">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" style="cursor: pointer;position: absolute; width: 1.25rem; right: 1rem; top: 1rem;">
+                          <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
+                          <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
+                        </svg>
+                      </div>
                     </div>
                     <button class="btn btn-custom-primary btn-block text-white btn-user" type="submit">LOGIN
                     </button>
@@ -45,5 +51,6 @@
   <?php include_once "_partials/scripts.php" ?>
 
 </body>
+<script src="<?= base_url('/assets/js/eye.js'); ?>"></script>
 
 </html>
